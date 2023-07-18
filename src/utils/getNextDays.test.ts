ae6fe9ec08811002b39846cb9caaf157d1b4de9e
RennanD <rplaykl@gmail.com>
2023-07-18 09:43:01 -0300

    Teste Unitário - Agrupando testes com Describe

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,7 +1,9 @@
 import { getNextDays } from './getNextDays';
 
-it("should be return the next five days", () => {
-  const days = getNextDays();
-
-  expect(days.length).toBe(5)
+describe('getNextDays', () => {
+  it("should be return the next five days", () => {
+    const days = getNextDays();
+  
+    expect(days.length).toBe(5)
+  })
 })
