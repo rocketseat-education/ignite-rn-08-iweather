5a98d0336adc7132fee88401129fac5de1915879
RennanD <rplaykl@gmail.com>
2023-07-18 10:40:30 -0300

    Teste de Componentes - Renderizando o componente no teste

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,5 +3,9 @@ import { render } from "@testing-library/react-native";
 import { Input } from "@components/Input"
 
 describe("Component: Input", () => {
-
+  it("should  be render without activity indicator", () => {
+    render(
+      <Input />
+    )
+  })
 })
