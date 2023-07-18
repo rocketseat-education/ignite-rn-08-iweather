e8a44ff42525578ebe887fa63fbca0c863dc24d8
RennanD <rplaykl@gmail.com>
2023-07-18 14:07:19 -0300

    Mocks - Mock de função

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -6,18 +6,22 @@ describe("Component: SelectList", () =>{
   it('should be return city details selected', async() => {
     const data = [
       { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
-      { id: '2', name: 'Campo grande', latitude: 123, longitude: 456 }
+      { id: '2', name: 'Campo grande', latitude: 789, longitude: 487 }
     ]
+
+    const onPress = jest.fn();
 
     render(
       <SelectList 
         data={data}
         onChange={() =>{}}
-        onPress={() => {}}
+        onPress={onPress}
       />
     )
 
     const selectedCity = screen.getByText(/campo/i)
     fireEvent.press(selectedCity)
+
+    expect(onPress).toBeCalledTimes(1)
   })
 })
