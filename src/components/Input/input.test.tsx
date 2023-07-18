b3dc96b0e644df848ee0d359a4c03b47dddc0b77
RennanD <rplaykl@gmail.com>
2023-07-18 11:26:35 -0300

    Teste de Componentes - Entendendo o Get e o Query

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -6,7 +6,7 @@ describe("Component: Input", () => {
   it("should  be render without activity indicator if isLoading prop is undefined", () => {
     render(<Input />)
 
-    const activityIndicator = screen.getByTestId('activity-indicator')
-    console.log(activityIndicator)
+    const activityIndicator = screen.queryByTestId('activity-indicator');
+    expect(activityIndicator).toBeNull();
   })
 })
