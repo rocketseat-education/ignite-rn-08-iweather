9fbeb8d5f5b882e3c5ad33704b3b151a443ea0de
RennanD <rplaykl@gmail.com>
2023-07-18 09:37:41 -0300

    Teste Unitário - Utilizar test ou it

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,6 +1,6 @@
 import { getNextDays } from './getNextDays';
 
-test("should be return the next five days", () => {
+it("should be return the next five days", () => {
   const days = getNextDays();
 
   expect(days.length).toBe(5)
