153e637fd169cd1ab63ac2c70da16652e770e254
RennanD <rplaykl@gmail.com>
2023-07-18 11:29:58 -0300

    Teste de Componentes - toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -9,4 +9,11 @@ describe("Component: Input", () => {
     const activityIndicator = screen.queryByTestId('activity-indicator');
     expect(activityIndicator).toBeNull();
   })
+
+  it("should  be render with activity indicator if isLoading prop is true", () => {
+    render(<Input isLoading />)
+
+    const activityIndicator = screen.getByTestId('activity-indicator');
+    expect(activityIndicator).toBeTruthy();
+  })
 })
