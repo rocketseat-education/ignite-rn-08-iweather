17852d9accb678c1a15ccb4783cbf92b4ded9849
RennanD <rplaykl@gmail.com>
2023-07-19 15:09:47 -0300

    Testando Hooks - Utilizando o findByText

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,12 +1,14 @@
-import { render } from "@testing-library/react-native"
+import { render, screen, waitFor } from "@testing-library/react-native"
 import { Routes } from "."
 
 describe("Routes", () => {
-  it('should be render Search screen when not city selecte', () => {
-    const { debug } = render(
+  it('should be render Search screen when not city selecte', async () => {
+    render(
       <Routes />
     )
 
-    debug()
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+
+    expect(title).toBeTruthy()
   })
 })
