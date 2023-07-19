6def8748b0f4402411ca12513f58e0bd391e46cf
RennanD <rplaykl@gmail.com>
2023-07-19 11:44:24 -0300

    Mocks - Teste do saveStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,25 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { CityProps } from "@services/getCityByNameService"
 
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
     const response = await getStorageCity()
 
     expect(response).toBeNull()
+  })
+
+  it('shoul be return city storaged', async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name:'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+    
+    await saveStorageCity(newCity)
+
+    const response = await getStorageCity()
+
+    expect(response).toEqual(newCity)
   })
 })
