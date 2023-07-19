1c8411418b441f4d424a7a61f4c34224dc519825
RennanD <rplaykl@gmail.com>
2023-07-19 11:59:14 -0300

    Mocks - Testando o componente WeatherItem

---
+++ src/components/Day/day.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react-native"
+import { Day } from "@components/Day"
+
+import clearDay from '@assets/clear_day.svg'
+
+describe('Component: Day', () => {
+  it('should be render day', () => {
+    const { debug } = render(
+      <Day 
+        data={{ 
+          day: '18/07',
+          min: '30°c',
+          max: '34°c',
+          icon: clearDay,
+          weather: 'Céu limpo'
+        }}
+      />
+    )
+
+    debug();
+  })
+})
