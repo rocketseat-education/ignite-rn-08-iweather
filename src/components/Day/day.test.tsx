de498d47dba16209d79922c128da2e21d1f99bf9
RennanD <rplaykl@gmail.com>
2023-07-19 12:03:49 -0300

    Mocks - Finalizando o teste do componente Day

--- src/components/Day/day.test.tsx
+++ src/components/Day/day.test.tsx
@@ -1,11 +1,11 @@
-import { render } from "@testing-library/react-native"
+import { render, screen } from "@testing-library/react-native"
 import { Day } from "@components/Day"
 
 import clearDay from '@assets/clear_day.svg'
 
 describe('Component: Day', () => {
   it('should be render day', () => {
-    const { debug } = render(
+    render(
       <Day 
         data={{ 
           day: '18/07',
@@ -17,6 +17,6 @@ describe('Component: Day', () => {
       />
     )
 
-    debug();
+    expect(screen.getByText('18/07')).toBeTruthy()
   })
 })
