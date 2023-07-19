3da3e285fc3606338a15216bf9221fbb63754e61
RennanD <rplaykl@gmail.com>
2023-07-19 16:43:05 -0300

    Teste de integração - Testando o Dashboard

---
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -0,0 +1,25 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
+import { render, screen, waitFor } from "@__tests__/utils/customRender"
+import { api } from "@services/api"
+import { Dashboard } from "@screens/Dashboard"
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+
+describe("Screen: Dashboard", () => {
+  it('should be show city weather', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const city = {
+      id: '1',
+      name: 'Rio do Sul, BR',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+
+    render(<Dashboard />)
+
+    const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
+    expect(cityName).toBeTruthy() 
+  })
+})
