728e613bd4d65d22fb0817f0911c8302818e5ee4
RennanD <rplaykl@gmail.com>
2023-07-19 14:48:31 -0300

    Testando Hooks - Wrapper

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -1,10 +1,11 @@
 import { renderHook } from '@testing-library/react-native'
 
 import { useCity } from '@hooks/useCity'
+import { CityProvider } from '@contexts/CityContext'
 
 describe('Context: cityContext', () => {
   it('should be change selected city',  () => {
-    const { result } = renderHook(() => useCity())
-    console.log(result.current.city)
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
+    console.log(result.current)
   })
 })
